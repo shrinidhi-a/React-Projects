88e67d3e846bce2fa98b7eebcdf813ae861ca4db
Shri6 <shrinidhisringeri@gmail.com>
2023-12-27 11:36:01 +0530

    A Counter Which uses UserState

---
+++ 01-Counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  // let counter = 15;
+  const [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    // counter = counter + 1;
+    setCounter(counter + 1 )
+
+  }
+
+  const removeValue = () => {
+    // counter = counter + 1;
+    setCounter(counter - 1 )
+
+  }
+
+  return (
+    <>
+      <h1> Counter Value: {counter} </h1>
+      <button onClick={addValue}> Add Value </button> {"  "}
+      <button onClick={removeValue}> Remove Value </button>
+    </>
+  )
+}
+
+export default App
